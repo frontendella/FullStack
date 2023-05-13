4f1c714e9e3c98c999689fc3a34ba3ece7163d81
frontendella <ryukhinaella@gmail.com>
2023-05-12 19:21:32 -0700

    first commit

---
+++ 02. Authentication with Google OAuth/01. Testing OAuth/config/keys.js
@@ -0,0 +1,11 @@
+module.exports = {
+    googleClientID: '857411769252-a03m8s6baq2hgbqv5408eg4mdsj5c3jo.apps.googleusercontent.com',
+    googleClientSecret: 'KmBjJxohgQFJhhDhy0N2O46l',
+    //mongoURI: 'mongodb://theda:obscured1@ds043605.mlab.com:43605/passport',
+    mongoURI: 'mongodb+srv://p00gz:JyKbVf7l6ATUk54h@emaily-uwsj6.mongodb.net/test?retryWrites=true',
+    cookieKey: 'jlkjfdklajfdajiejijijfdf',
+    stripePublishableKey: 'pk_test_JMdyKVvf8EGTB0Fl28GsN7YY',
+    stripeSecretKey: 'sk_test_hnfrAm8rOkryFEnV23jjfFlw',
+    sendGridKey: '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',
+    redirectDomain: 'http://localhost:3000'
+}
